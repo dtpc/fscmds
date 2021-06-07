43ae25d36facf62f4595800aebd74752bd440f42
Dave Cole <david.cole@data61.csiro.au>
2021-06-07 13:13:56 +1000

    Initial commit

---
+++ fstree/cli.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+from pathlib import PurePath
+
+import click
+from fsspec.core import url_to_fs
+from treelib import Tree
+
+
+@click.command()
+@click.argument("uri")
+@click.option(
+    "-L",
+    "level",
+    metavar="level",
+    type=int,
+    default=None,
+    help="Descend only level directories deep.",
+)
+def main(uri, level):
+    fs, root = url_to_fs(uri)
+    root = PurePath(root)
+    t = Tree()
+
+    def walk(path, parent=None, level=100):
+        t.create_node(path.name, str(path), parent=parent)
+        if level and fs.isdir(path):
+            for s in sorted(fs.ls(path)):
+                walk(PurePath(s), parent=str(path), level=level - 1)
+
+    walk(root, level=level)
+
+    t.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+tests_require = [
+    'black>=20.8b1',
+    'flake8>=3.8.0',
+    'flake8-isort>=4.0.0',
+    'isort>=5.1.0',
+]
+
+
+setup(
+    name='fstree',
+    author='dtpc',
+    license='MIT License',
+    description="CLI tool to print a filesystem tree",
+    python_requires='>=3.8.0',
+    packages=find_packages(
+        exclude=('tests')
+    ),
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'click>=5.0',
+        'fsspec',
+        'treelib',
+    ],
+    extras_require={'test': tests_require},
+    tests_require=tests_require,
+    entry_points={
+        'console_scripts': [
+            'fstree = fstree.cli:main',
+        ],
+    },
+)
